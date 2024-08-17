7b2f79545a853b0a8523ba5c8eeaf1e1c3fa2eab
vishwajit <vishwajit.r@media.net>
2024-08-17 14:48:44 +0530

    files added

---
+++ server.js
@@ -0,0 +1,29 @@
+// server.js
+const express = require('express');
+const bodyParser = require('body-parser');
+const fs = require('fs');
+const path = require('path');
+
+const app = express();
+const port = 3000;
+
+app.use(bodyParser.json());
+
+app.post('/track', (req, res) => {
+  const data = req.body;
+
+  // Log data to a file in JSON format
+  const logFilePath = path.join(__dirname, 'clicks.json');
+  fs.appendFile(logFilePath, JSON.stringify(data) + '\n', (err) => {
+    if (err) {
+      console.error('Error writing to file:', err);
+      res.status(500).send('Internal Server Error');
+    } else {
+      res.status(200).send('Click tracked successfully');
+    }
+  });
+});
+
+app.listen(port, () => {
+  console.log(`Server is running on http://localhost:${port}`);
+});
